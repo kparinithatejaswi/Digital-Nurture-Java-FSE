f7904eacce322c562b717a4a99507cc2dac4f96d
Parinitha Tejaswi Koripalli <kparinithatejaswi@gmail.com>
2026-06-19 10:51:46 +0530

    Added Singleton Pattern Exercise

---
+++ Logger.java
@@ -0,0 +1,21 @@
+public class Logger {
+    private static Logger instance = new Logger();
+
+    private Logger()
+    {
+        System.out.println("Logger Object Created");
+    }
+
+    public static Logger getInstance()
+    {
+        return instance;
+    }
+
+    public void log(String message)
+    {
+        System.out.println("LOG MESSAGE : " + message);
+    }
+
+}
+    
+
---
+++ SingletonTest.java
@@ -0,0 +1,34 @@
+public class SingletonTest {
+      public static void main(String[] args)
+    {
+
+        Logger logger1 = Logger.getInstance();
+
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application Started");
+
+        logger2.log("User Logged Successfully");
+
+
+
+        if(logger1 == logger2)
+        {
+            System.out.println(
+            "Both objects are same. Singleton Pattern implemented successfully"
+            );
+        }
+
+        else
+        {
+            System.out.println(
+            "Different objects created"
+            );
+        }
+
+
+    }
+
+}
+    
+
