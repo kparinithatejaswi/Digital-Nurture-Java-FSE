b4b3530428c7358e8b5d1143c3d41a1b231aa16c
Parinitha Tejaswi Koripalli <kparinithatejaswi@gmail.com>
2026-07-10 14:49:02 +0530

    added JUnitExercise3

---
+++ JUnitExercise3/src/main/java/com/digitalnurture/JUnitExercise3/App.java
@@ -0,0 +1,10 @@
+package com.digitalnurture.JUnitExercise3;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
